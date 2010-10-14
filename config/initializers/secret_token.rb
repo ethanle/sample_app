340a50f933cbdb918920ea371814ddebb18cfcd2
ethan.le <ethan.le@ufinity.com>
2010-10-14 15:40:59 +0800

    init git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6cc43213d9cf10c68e2977b978a0f1c9e9664bfe2af8391757107ba5c4cfa7cbd57ec5be683d6a264502a3130cea07942fb0fa6d51840f84eebea6e5b73a9f86'
